0e13e27823b737d48efa3e601b8ecd4fef9094b4
Olaniyan-tech <alliolaniyan1@gmail.com>
2025-09-09 09:28:24 -0400

    Changed settings.py

--- alexproject/settings.py
+++ alexproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ep+my09!c%nv^(u3u4ar1818==&v22j#kkk2!)d#=8x$a3a=2o')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
